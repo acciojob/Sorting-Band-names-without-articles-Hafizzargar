f286e3e466413463aaa7ad12e4764573e5aa0642
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2023-09-23 00:49:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
+let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+let re=/\bthe\b|\ban\b|\ba\b/g;
+let ar=[];
+for(let i=0;i<arr.length;i++){
+	let arl=arr[i].replace(re,"").trim();
+	ar.push(ar).push(arl);
+}
