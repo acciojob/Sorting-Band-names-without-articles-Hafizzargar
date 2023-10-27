f05fe231725a89dc52398cf6091b156ae0a888ef
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2023-10-28 02:03:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 //your code here
 let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-let mp={};
-let re=/\bthe\b|\ban\b|\ba\b/g;
-let ar=[];
-for(let i=0;i<arr.length;i++){
-	let arl=arr[i].replace(re,"").trim();
-	ar.push(ar).push(arl);
-}
-ar.sort();
+
+arr.sort();
 let ans=[];
-for(let i of ar){
-	ans.push{mp[i]};
-}
+
 console.log(ans);
